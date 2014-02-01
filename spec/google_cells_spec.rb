0caf3cb7735b1c4c58a26f3607a56b89d84d1567
Jessica Megan Thrower <jmthrower@gmail.com>
2014-02-01 14:40:23 -0500

    Added test for config

--- spec/google_cells_spec.rb
+++ spec/google_cells_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe GoogleCells do
+
+  it "can be configured" do
+    email = "test@test.com"
+    GoogleCells.configure do |config|
+      config.service_account_email = email
+    end
+
+    GoogleCells.config.service_account_email.should eq email
+  end
 end

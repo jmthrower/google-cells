d8e713e96199d58ab89cadbecaef4770d399a99e
Jessica Megan Thrower <jmthrower@gmail.com>
2014-02-02 13:30:58 -0500

    Moved client iniialization to config

--- lib/google_cells.rb
+++ lib/google_cells.rb
@@ -4,12 +4,29 @@ require "google_cells/spreadsheet"
 
 module GoogleCells
   class << self
-    attr_accessor :config
+    attr_accessor :config, :client
   end
 
   def self.configure
     self.config ||= Configuration.new
     yield(config)
+
+    key = Google::APIClient::KeyUtils.load_from_pkcs12(config.key_file, 
+      config.key_secret)
+
+    self.client = Google::APIClient.new(
+      :application_name => 'GoogleCells App',
+      :application_version => '0.0.1')
+
+    client.authorization = Signet::OAuth2::Client.new(
+      :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
+      :audience => 'https://accounts.google.com/o/oauth2/token',
+      :scope => ['https://www.googleapis.com/auth/drive',
+        'https://spreadsheets.google.com/feeds'],
+      :issuer => config.service_account_email,
+      :signing_key => key)
+
+    config
   end
 
   class Configuration
--- lib/google_cells/client.rb
+++ lib/google_cells/client.rb
@@ -1,30 +1,13 @@
 require 'google_cells'
 require 'google/api_client'
+require "cgi"
 
 module GoogleCells
 
   class Client
-    
-    def initialize()
-      config = GoogleCells.config
-      key = Google::APIClient::KeyUtils.load_from_pkcs12(config.key_file, 
-        config.key_secret)
-
-      @client = Google::APIClient.new(
-        :application_name => 'GoogleCells App',
-        :application_version => '0.0.1')
-
-      @client.authorization = Signet::OAuth2::Client.new(
-        :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
-        :audience => 'https://accounts.google.com/o/oauth2/token',
-        :scope => ['https://www.googleapis.com/auth/drive',
-          'https://spreadsheets.google.com/feeds'],
-        :issuer => config.service_account_email,
-        :signing_key => key)
-    end
 
     def authorize!
-      @client.authorization.fetch_access_token!
+      GoogleCells.client.authorization.fetch_access_token!
     end
   end
 end
